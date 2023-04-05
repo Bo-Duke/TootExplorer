5c580995bee6ef96dc5cd68ad10055f3aff68e3f
Maxime Bouveron <maxime.bouveron@gmail.com>
2023-04-05 12:32:32 +0200

    Fixing issue with instance list rendering

--- components/InstancesList.vue
+++ components/InstancesList.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="instances.length > 0" class="grid grid-cols-1 gap-6 lg:gap-8 sm:grid-cols-2 lg:grid-cols-3 mt-5">
-    <instance :v-for="instanceObject in instances" :key="instanceObject.domain" :instance="instanceObject" />
+    <instance v-for="instanceObject in instances" :key="instanceObject.domain" :instance="instanceObject" />
   </div>
   <no-followings-error v-else :instance="instance" />
 </template>
