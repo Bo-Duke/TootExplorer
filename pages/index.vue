f3d5ce956c5f9c2316e4ca6eea6dd13d47086e20
Maxime Bouveron <maxime.bouveron@gmail.com>
2023-04-05 15:34:01 +0200

    Don't show sort select if no instances

--- pages/index.vue
+++ pages/index.vue
@@ -64,7 +64,7 @@
     </form>
     <user-info v-if="userDetails" :key="userDetails.id" :details="userDetails" :user="user" />
     <general-error v-if="isError" />
-    <div >
+    <div v-if="instanceList?.length > 0">
       <label for="sorting" class="text-left block my-2 text-sm font-medium text-gray-900 dark:text-white">Sorting</label>
       <select @change="sortInstances" v-model="sorting" id="sorting" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option value="smart">Smart (Default)</option>
